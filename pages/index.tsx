feba0221bf693af05d40592c5c2d4c49e8a50b40
codeyash02@bitbucket.org <ravipathak3001@gmail.com>
2022-06-01 18:59:49 +0530

    fourth commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,12 @@ import styles from "../styles/Home.module.css";
 import Header from '../components/Header';
 import Banner from "../components/Banner";
 import Features from "../components/Features";
-// import Head from "next/head"
+import Head from "next/head"
 import Security from "../components/Security";
 import WhyImageDiv from "../components/WhyImageDiv";
 import Setup from "../components/Setup";
 import Thanks from "../components/Thanks"
-import { Html, Head, Main, NextScript } from 'next/document'
+// import { Html, Head, Main, NextScript } from 'next/document'
 
 
 
@@ -15,12 +15,12 @@ export default function document() {
   
   return (
     <>
-      <Html>
+      {/* <Html> */}
         
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Racing+Sans+One&display=swap" rel="stylesheet" />
+       
       </Head>
 
 
@@ -37,7 +37,7 @@ export default function document() {
         
   
   
-      </Html>
+      {/* </Html> */}
     </>
   );
 }
